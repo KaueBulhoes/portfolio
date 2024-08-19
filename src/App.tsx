c71f5b0e7608678dbd1dd297813c418842683807
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2024-08-19 16:33:40 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Teste from './task'
+import EstiloGlobal from './styles'
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Teste />
+    </>
+  )
+}
+
+export default App
