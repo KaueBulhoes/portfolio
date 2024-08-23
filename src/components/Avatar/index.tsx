b5ff20d14bd5a114556dcf6dac4ba690a3fafbc3
Kaue <130503651+KaueBulhoes@users.noreply.github.com>
2024-08-23 11:50:32 -0300

    .

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/kauebulhoes.png" />
+
+export default Avatar
--- src/containers/Siderbar/index.tsx
+++ src/containers/Siderbar/index.tsx
@@ -1,9 +1,19 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Siderbar = () => (
   <aside>
-    <img src="https://github.com/kauebulhoes.png" />
-    <Titulo fontSize={20}>Kaue Bulhoes</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Kaue Bulhoes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        kauebulhoes
+      </Paragrafo>
+      <Descricao fontSize={12}>Engenheiro Front End</Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
